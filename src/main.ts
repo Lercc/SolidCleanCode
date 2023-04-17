8ef6c502a8a0c089456b8e866dfb498eaf767932
lercc <lercc.en@gmail.com>
2023-04-16 20:48:32 -0500

    practica alto - bajo acoplamiento

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
-import './clean-code/07-tarea-b'
+// import './clean-code/07-tarea-b'
+import './code-smells/02-low-coupling'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
